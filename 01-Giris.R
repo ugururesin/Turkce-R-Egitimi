2a3402da785cf018916bd21dbd1a67ac3098d61c
ugur <55594186+ugururesin@users.noreply.github.com>
2021-03-06 15:26:44 +0300

    Create 01-Giris.R

---
+++ 01-Giris.R
@@ -0,0 +1,30 @@
+# R PROGRAMLAMA NOTLARI - G??R????
+
+# AR??TMET??K OPERAT??RLER
+5-1
+5+2
+3*2
+8/2
+
+# ATAMA OPERAT??RLER??
+ogrenci_sayisi = 50
+ogrenci_sayisi <- 50
+50 -> ogrenci_sayisi
+(ogrenci_sayisi = 50)
+
+# VER?? T??PLER??
+x = 100
+class(x)
+
+y = 3.5
+class(y)
+
+z = "Ali"
+class(z)
+
+w = TRUE
+class(w)
+
+w = FALSE
+class(w)
+
